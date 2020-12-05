9cb9240101d5eeae74efe3918747152adc7db731
rishabh1897 <singh.rishabh.1897@gmail.com>
2020-12-05 23:55:00 +0530

    feat-added input output

---
+++ app.js
@@ -0,0 +1,30 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#input-txt");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslation(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something went wrong with server! try again after sometime")
+}
+
+function clickHandler() {
+    var inputTxt = txtInput.value; //takin input
+
+    //callin server for processing
+    fetch(getTranslation(inputTxt))
+        .then(response => response.json())
+        .then(json => {
+            var translatedTxt = json.contents.translated;
+            outputDiv.innerHTML = translatedTxt;
+        })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", function () {
+    })
